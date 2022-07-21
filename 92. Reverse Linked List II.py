18cabf0c8204c8c24cfc16e883b25ef5e586c546
Depender Kumar Soni <dependerkumarsoni@gmail.com>
2022-07-21 10:01:16 +0530

    July 21, 2022

---
+++ 92. Reverse Linked List II.py
@@ -0,0 +1,27 @@
+# Question: https://leetcode.com/problems/reverse-linked-list-ii/
+
+# Solution for Leetcode:
+
+class Solution:
+    def reverseBetween(self, head, left: int, right: int):
+        # to stores the values that needed to be reverse.
+        values = []
+        # to keep track of the nodes which needs to be reverse.
+        nodes = []
+
+        counter = 0
+        curr = head
+        while curr:
+            counter += 1
+            if counter >= left and counter <= right:
+                # Storing Values and Nodes between Left and Right.
+                values.append(curr.val)
+                nodes.append(curr)
+            curr = curr.next
+        
+        # Overwriting values of nodes from stored values and nodes.
+        for node in nodes:
+            node.val = values.pop(-1)
+
+        # Returning the head.
+        return head
